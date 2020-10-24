d21c73de1eee331badcc73c687dcb83d27bf0259
Your Name <gourav.saini.2703@gmail.com>
2020-10-24 05:37:59 +0200

    Graph added

--- db.py
+++ db.py
@@ -1,11 +1,21 @@
 import pymongo
 import scraper
 #print(scraper.bundesweit)
+x_array = [] 
+y_array = []
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
 mycol = mydb["bundesland_corona_inzident"]
 mydict = scraper.bundesweit
 x = mycol.insert_one(mydict)
 for y in mycol.find():
-  print(y)
+   a = int(y['datum'][0:2])
+   b = float((y['zahlen'][4]['inzidenz']).replace("," , "."))
+   x_array.append(a)
+   y_array.append(b)
+
+#print ( x_array,y_array)
+
+
+
 
---
+++ display.py
@@ -0,0 +1,30 @@
+from PyQt5 import QtWidgets
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
+import sys  # We need sys so that we can pass argv to QApplication
+import os
+import db
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.graphWidget = pg.PlotWidget()
+        self.setCentralWidget(self.graphWidget)
+
+        Inzident = db.y_array
+        datum = db.x_array
+        print (db.x_array , db.y_array)
+        # plot data: x, y values
+        self.graphWidget.plot(datum,Inzident)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
